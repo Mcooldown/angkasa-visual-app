5c5953ff30790fd84bdc6dd87690b98bcd596911
Mcooldown <vincenthadinata30@gmail.com>
2021-10-12 16:51:12 +0700

    rapihin structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+import '@fortawesome/fontawesome-free/scss/brands.scss';
+import '@fortawesome/fontawesome-free/scss/solid.scss';
+import '@fortawesome/fontawesome-free/scss/regular.scss';
+import { Home } from './pages';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+          <Route path="/" exact>
+                <Home />
+          </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+const Home = () => {
+     return (
+          <div>
+               INI HOME               
+          </div>
+     )
+}
+
+export default Home
